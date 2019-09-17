95346a02595a8500563809b48c2a72de392ab7d4
eneax <19818937+eneax@users.noreply.github.com>
2019-09-17 09:32:42 +0200

    Add useState to counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
+  const [count, setCount] = useState(0)
+
   return (
     <div className='counter'>
       <h3>click the button if you like the post</h3>
-      <h4>likes: </h4>
-      <button>click me!</button>
+      <h4>likes: {count}</h4>
+      <button onClick={() => setCount(count + 1)}>click me!</button>
     </div>
   )
 }
