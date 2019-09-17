8256698e26381e92871babbb39b3c98f9df7f719
eneax <19818937+eneax@users.noreply.github.com>
2019-09-17 09:28:37 +0200

    Create counter component

---
+++ src/components/counter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div className='counter'>
+      <h3>click the button if you like the post</h3>
+      <h4>likes: </h4>
+      <button>click me!</button>
+    </div>
+  )
+}
+
+export default Counter
