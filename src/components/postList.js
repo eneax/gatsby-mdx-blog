2a7a625f74bc1c2d4b0feb5c6f55e4b75d6de5b4
eneax <19818937+eneax@users.noreply.github.com>
2019-09-23 11:25:20 +0200

    Create PostList component

---
+++ src/components/postList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PostCard from './postCard'
+
+const PostList = () => {
+  return (
+    <div>
+      PostList component
+
+      <PostCard />
+    </div>
+  )
+}
+
+export default PostList
