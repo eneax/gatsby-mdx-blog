966c8be92010568e0468df38a9e5737c7c22252f
eneax <19818937+eneax@users.noreply.github.com>
2019-09-23 11:25:44 +0200

    Import PostList in home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from "react"
+import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import PostList from '../components/postList'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <p>Welcome to your new Gatsby site.</p>
-    <Link to='/post'>Post page</Link>
+    <SEO title='Home' />
+    
+    <PostList />
   </Layout>
 )
 
