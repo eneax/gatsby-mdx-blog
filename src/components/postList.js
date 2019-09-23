e63c6d30f498d66d2fcf3f9d5fe599f8b65daa3c
eneax <19818937+eneax@users.noreply.github.com>
2019-09-23 11:46:20 +0200

    Get posts props in PostList component

--- src/components/postList.js
+++ src/components/postList.js
@@ -1,13 +1,23 @@
 import React from 'react'
+
+import styles from './postList.module.css'
 import PostCard from './postCard'
 
-const PostList = () => {
+const PostList = ({ posts }) => {
   return (
-    <div>
-      PostList component
+    <section className={styles.posts}>
+      <h1>John Doe</h1>
+      <h4>Personal Blog</h4>
 
-      <PostCard />
-    </div>
+      <div className={styles.center}>
+        {posts.map(({ node }, index) => (
+          <PostCard 
+            key={index}
+            post={node}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
 
