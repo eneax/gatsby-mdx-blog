169a96df8e19ad25beaa36ba32052b730cefb1b6
eneax <19818937+eneax@users.noreply.github.com>
2019-09-23 11:32:12 +0200

    Setup graphql query for getting posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,48 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import PostList from '../components/postList'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title='Home' />
-    
-    <PostList />
-  </Layout>
-)
+const IndexPage = () => {
+  const { posts } = useStaticQuery(getPosts)
+
+  return (
+    <Layout>
+      <SEO title='Home' />
+      
+      <PostList posts={posts} />
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+
+// query
+const getPosts = graphql`
+  {
+    posts: allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+            slug
+            date
+            author
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
