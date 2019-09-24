c945d815e191167828c22f55a683d8e567aa8cd2
eneax <19818937+eneax@users.noreply.github.com>
2019-09-24 09:56:09 +0200

    Create pages programmatically

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,31 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
 
-// You can delete this file if you're not using it
+  // query for posts
+  const { data: { allMdx: { edges: posts } } } = await graphql(`
+    {
+      allMdx {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  // create a page for each item in the array
+  posts.forEach(({ node }) => {
+    const { slug } = node.frontmatter 
+
+    createPage({
+      path: slug,
+      component: require.resolve('./src/templates/postTemplate.js'),
+      context: {
+        slug: slug
+      }
+    })
+  });
+}
