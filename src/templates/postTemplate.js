6bf0d90e8e279fa64a95a927e82b1ea0f92209b9
eneax <19818937+eneax@users.noreply.github.com>
2019-09-24 09:38:56 +0200

    Setup postTemplate

---
+++ src/templates/postTemplate.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Img from 'gatsby-image'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
+import styles from './postTemplate.module.css'
+import Layout from '../components/layout'
+
+
+const PostTemplate = () => {
+  return (
+    <Layout>
+      this is a PostTemplate
+    </Layout>
+  )
+}
+
+export default PostTemplate
