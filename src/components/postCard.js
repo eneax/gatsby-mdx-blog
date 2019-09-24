1b12d15c52f502df7002104ce88bffdcd5281a45
eneax <19818937+eneax@users.noreply.github.com>
2019-09-24 09:24:41 +0200

    Add PostCard styles

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
-const PostCard = () => {
+import styles from './postCard.module.css'
+
+
+const PostCard = ({ post }) => {
+  const { title, date, author, slug } = post.frontmatter
+  const img = post.frontmatter.image.childImageSharp.fluid
+
   return (
-    <div>
-      PostCard component
-    </div>
+    <article className={styles.card}>
+      <div className={styles.image}>
+        <Img fluid={img} />
+      </div>
+      <div className={styles.info}>
+        <div>
+          <h2>{title}</h2>
+          <h6>
+            <span>by {author}</span> / <span>{date}</span>
+          </h6>
+          <p>{post.excerpt}</p>
+          <Link 
+            to={slug}
+            className={styles.link}
+          >
+            Read More
+          </Link>
+        </div>
+      </div>
+    </article>
   )
 }
 
