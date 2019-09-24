da9dbc5599c5158c8004a13432930b8e55924ac5
eneax <19818937+eneax@users.noreply.github.com>
2019-09-24 11:32:27 +0200

    Setup getPostQuery in PostTemplate

--- src/templates/postTemplate.js
+++ src/templates/postTemplate.js
@@ -7,12 +7,56 @@ import styles from './postTemplate.module.css'
 import Layout from '../components/layout'
 
 
-const PostTemplate = () => {
+const PostTemplate = ({ data }) => {
+  const { title, date, author, image } = data.mdx.frontmatter
+  const { body } = data.mdx
+  const img = image.childImageSharp.fluid
+
   return (
     <Layout>
-      this is a PostTemplate
+      <section className={styles.template}>
+        <Link
+          to='/'
+          className={styles.link}
+        >
+          Back to all posts
+        </Link>
+
+        <div className={styles.info}>
+          <h1>{title}</h1>
+          <h4>
+            <span>by {author}</span> / <span>{date}</span>
+          </h4>
+        </div>
+
+        <Img fluid={img} />
+      </section>
     </Layout>
   )
 }
 
 export default PostTemplate
+
+
+// query
+export const getPostQuery = graphql`
+  # $slug --> comes from the gatsby-node file (context)
+  query getPost($slug: String!) {
+    mdx(frontmatter: {slug: {eq: $slug}}) {
+      frontmatter {
+        title
+        slug
+        date
+        author
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      body
+    }
+  }
+`
