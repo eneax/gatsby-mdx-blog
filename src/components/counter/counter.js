74b8286ee662846a34c428eae168fbcc2ae9d685
eneax <19818937+eneax@users.noreply.github.com>
2019-10-20 17:30:37 +0200

    Refactor counter

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import styles from './counter.module.css'
 
 const Counter = () => {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='counter'>
+    <div className={styles.counter}>
       <h3>click the button if you like the post</h3>
       <h4>likes: {count}</h4>
       <button onClick={() => setCount(count + 1)}>click me!</button>
